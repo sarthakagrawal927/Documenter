0040684580d91a20dcfba973d2ddd5c92f2b0439
Sarthak Agrawal <sarthakagrawal927@gmail.com>
2021-05-02 16:02:23 +0530

    still not working becasue of website issues

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,5 @@
-import { useState, useEffect, useRef } from "react";
 import * as esbuild from "esbuild-wasm";
+import { useState, useEffect, useRef } from "react";
 import { unpkgPathPlugin } from "../plugins/unpkg-path-plugin";
 import { fetchPlugin } from "../plugins/fetch-plugin";
 
@@ -8,19 +8,20 @@ const App = () => {
   const [input, setInput] = useState("");
   const [code, setCode] = useState("");
 
-  const startservice = async () => {
+  const startService = async () => {
     ref.current = await esbuild.startService({
       worker: true,
       wasmURL: "https://unpkg.com/esbuild-wasm@0.8.27/esbuild.wasm",
     });
   };
-
   useEffect(() => {
-    startservice();
+    startService();
   }, []);
 
   const onClick = async () => {
-    if (!ref.current) return;
+    if (!ref.current) {
+      return;
+    }
 
     const result = await ref.current.build({
       entryPoints: ["index.js"],
@@ -28,25 +29,22 @@ const App = () => {
       write: false,
       plugins: [unpkgPathPlugin(), fetchPlugin(input)],
       define: {
-        "process.env.NODE_ENV": "'production'",
+        "process.env.NODE_ENV": '"production"',
         global: "window",
       },
     });
-    // console.log(result);
     setCode(result.outputFiles[0].text);
   };
 
   return (
     <div>
       <textarea
-        name=''
-        id=''
         value={input}
         onChange={(e) => setInput(e.target.value)}></textarea>
-      <button onClick={onClick}>Submit</button>
+      <div>
+        <button onClick={onClick}>Submit</button>
+      </div>
       <pre>{code}</pre>
     </div>
   );
 };
-
-export default App;
--- src/plugins/unpkg-path-plugin.ts
+++ src/plugins/unpkg-path-plugin.ts
@@ -4,24 +4,25 @@ export const unpkgPathPlugin = () => {
   return {
     name: "unpkg-path-plugin",
     setup(build: esbuild.PluginBuild) {
-      //Handle root entry index.js
+      // Handle root entry file of 'index.js'
       build.onResolve({ filter: /(^index\.js$)/ }, () => {
         return { path: "index.js", namespace: "a" };
       });
 
-      // ../ or ./, Handle relative paths
+      // Handle relative paths in a module
       build.onResolve({ filter: /^\.+\// }, (args: any) => {
         return {
+          namespace: "a",
           path: new URL(args.path, "https://unpkg.com" + args.resolveDir + "/")
             .href,
-          namespace: "a",
         };
       });
-      //Handle main file
+
+      // Handle main file of a module
       build.onResolve({ filter: /.*/ }, async (args: any) => {
         return {
-          path: `https://unpkg.com/${args.path}`,
           namespace: "a",
+          path: `https://unpkg.com/${args.path}`,
         };
       });
     },
