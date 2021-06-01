1f7790f99947e0e36a4321ec31caf37e67f35016
Sarthak Agrawal <sarthakagrawal927@gmail.com>
2021-06-01 11:07:56 +0530

    getting some error

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,15 @@
+import "bulmaswatch/superhero/bulmaswatch.min.css";
 import * as esbuild from "esbuild-wasm";
 import { useState, useEffect, useRef } from "react";
 import { unpkgPathPlugin } from "../plugins/unpkg-path-plugin";
 import { fetchPlugin } from "../plugins/fetch-plugin";
+import CodeEditor from "../components/code-editor";
 
 const App = () => {
   const ref = useRef<any>();
   const iframe = useRef<any>();
-
   const [input, setInput] = useState("");
+  const [code, setCode] = useState("");
 
   const startService = async () => {
     ref.current = await esbuild.startService({
@@ -15,7 +17,6 @@ const App = () => {
       wasmURL: "https://unpkg.com/esbuild-wasm@0.8.27/esbuild.wasm",
     });
   };
-
   useEffect(() => {
     startService();
   }, []);
@@ -38,44 +39,44 @@ const App = () => {
       },
     });
 
+    // setCode(result.outputFiles[0].text);
     iframe.current.contentWindow.postMessage(result.outputFiles[0].text, "*");
   };
 
   const html = `
-  <html>
-  <head></head>
-  <body>
-    <div id="root"></div>
-    <script>
-      window.addEventListener('message', (event) => {
-       try{ eval(event.data)}catch(err){
-           const root = document.querySelector('#root');
-           root.innerHTML = '<div style="color:red;">'+err.message+'</div>';
-           console.error(err);
-       }
-      }, false);
-    </script>
-  </body>
-  </html>
-`;
+    <html>
+      <head></head>
+      <body>
+        <div id="root"></div>
+        <script>
+          window.addEventListener('message', (event) => {
+            try {
+              eval(event.data);
+            } catch (err) {
+              const root = document.querySelector('#root');
+              root.innerHTML = '<div style="color: red;"><h4>Runtime Error</h4>' + err + '</div>';
+              console.error(err);
+            }
+          }, false);
+        </script>
+      </body>
+    </html>
+  `;
 
   return (
     <div>
+      <CodeEditor
+        initialValue='const a = 1;'
+        onChange={(value) => setInput(value)}
+      />
       <textarea
         value={input}
         onChange={(e) => setInput(e.target.value)}></textarea>
       <div>
         <button onClick={onClick}>Submit</button>
       </div>
-      <iframe
-        title='preview'
-        ref={iframe}
-        frameBorder='10'
-        sandbox='allow-scripts'
-        srcDoc={html}
-      />
-      {/* iframe will let us to make sure that our app and user's code remain in
-      defferent contexts */}
+      <pre>{code}</pre>
+      <iframe ref={iframe} sandbox='allow-scripts' srcDoc={html} />
     </div>
   );
 };
